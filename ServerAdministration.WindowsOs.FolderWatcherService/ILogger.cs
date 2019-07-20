1f40d3a264bfc107a728f882d1747b8cf3ab0d81
Hussein Shamekhi <agah.shamekhi@gmail.com>
2019-07-20 06:56:19 +0430

    Adding NLogger

---
+++ ServerAdministration.WindowsOs.FolderWatcherService/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerAdministration.WindowsOs.FolderWatcherService
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogInfo(object theObject);
+
+        void LogInfoAsync(string message);
+        void LogInfoAsync(object theObject);
+    }
+}
